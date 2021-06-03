1f2c6839847a3e098c069317a3d2090280527d76
Shamil <Shamil0710@mail.ru>
2021-06-03 11:27:44 +0300

    Init commit

---
+++ src/main/java/com/project/clickhouseproxyapi/ClickhouseProxyApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.clickhouseproxyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickhouseProxyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClickhouseProxyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/clickhouseproxyapi/ClickhouseProxyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.clickhouseproxyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickhouseProxyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
